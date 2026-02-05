71df10447efb725fc83ab376e67e5082f8b22037
Rishikant Patel <patelrishikant88@gamil.com>
2026-02-05 23:24:10 +0530

    build successful and final fixes

--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -2,14 +2,19 @@ import React from "react";
 
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
-function PricingPage() {
+
+function  SupportPage(){
   return (
     <>
+<Navbar />
       <Hero />
       <CreateTicket />
+  <Footer />
     </>
   );
 }
 
-export default PricingPage;
+export default SupportPage;
